328947181dce17b83c8ceb58e057a8737fc19065
yashcrest <yashcrest@gmail.com>
2023-08-12 11:51:12 +0930

    created a local http / tcp server for testing internal network

---
+++ two_servers/http_server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'content-type' : 'text/plain'});
+
+    res.end('Hello from http server\n');
+})
+
+server.listen(3002, '0.0.0.0');
---
+++ two_servers/tcp_server.js
@@ -0,0 +1,10 @@
+const net = require('net');
+
+//create tcp server
+const server = net.createServer((req, res) => {
+    Socket.write('Echo server\r\n')
+    Socket.pie(socket);
+})
+
+server.listen(3001, '0.0.0.0');
+
