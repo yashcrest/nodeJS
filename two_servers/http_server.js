022d808fa3966c77ea8a675d133125c7e3804103
yashcrest <yashcrest@gmail.com>
2023-11-16 19:07:59 +1030

    removing white spaces

--- two_servers/http_server.js
+++ two_servers/http_server.js
@@ -1,9 +1,8 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {'content-type' : 'text/plain'});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Hello from http server\n");
+});
 
-    res.end('Hello from http server\n');
-})
-
-server.listen(3002, '0.0.0.0');
+server.listen(3002, "0.0.0.0");
